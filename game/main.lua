be9030a40b8b309f1aad8d6c93db1d4eae3452b1
Ryan <rayaman99@gmail.com>
2017-11-29 22:13:32 -0500

    Basic Setup of files

---
+++ game/main.lua
@@ -0,0 +1,9 @@
+love.filesystem.setIdentity("love2dstreamtest")
+require("core.Utils")
+require("core.Library")
+require("bin")
+GLOBAL,sThread=require("multi.integration.loveManager").init()
+require("core.AudioManager")
+require("core.GuiManager")
+require("parseManager")
+gui.ff.Color=Color.Black
